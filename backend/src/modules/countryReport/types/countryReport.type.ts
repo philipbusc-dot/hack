d1cb12875aa7498a5948b72a24b531218c2f1262
nonthawatt.beem@gmail.com <nontawat.kitt@mail.kmutt.ac.th>
2026-05-30 13:10:17 +0700

    816-[FIX] schema ,type ,router

--- backend/src/modules/countryReport/types/countryReport.type.ts
+++ backend/src/modules/countryReport/types/countryReport.type.ts
@@ -6,7 +6,7 @@ export interface CountryReportInput {
     note:string;
 }
 export interface CountryReport extends CountryReportInput {
-    id: number
-    createAt: Date;
-    updateAt: Date;
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
 }
