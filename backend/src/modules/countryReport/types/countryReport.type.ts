51986e14d14e89b1cb446f03b9de7bc4097dc6f4
nonthawatt.beem@gmail.com <nontawat.kitt@mail.kmutt.ac.th>
2026-05-30 11:52:55 +0700

    816 - [ADD] countryReportModel

--- backend/src/modules/countryReport/types/countryReport.type.ts
+++ backend/src/modules/countryReport/types/countryReport.type.ts
@@ -5,7 +5,7 @@ export interface CountryReportInput {
     cases:number;
     note:string;
 }
-export interface CountryReport extends CountryReport {
+export interface CountryReport extends CountryReportInput {
     id: number
     createAt: Date;
     updateAt: Date;
